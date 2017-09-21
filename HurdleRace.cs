669e533000860f9a9ce3830ee38b8f097a13dcdb
Iti Tyagi <intechworx@gmail.com>
2017-09-21 14:45:39 +0530

    Update HurdleRace.cs

--- HurdleRace.cs
+++ HurdleRace.cs
@@ -1,16 +1,34 @@
-#include <cmath>
-#include <iostream>
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
 
-int main() {
-    size_t n, k;
-    std::cin >> n >> k;
-    
-    size_t tmp, max = 0;
-    while (n --> 0) {
-        std::cin >> tmp;
-        max = std::max(max, tmp);
+    static void Main(String[] args) {
+        string[] tokens_n = Console.ReadLine().Split(' ');
+        int n = Convert.ToInt32(tokens_n[0]);
+        int k = Convert.ToInt32(tokens_n[1]);
+        string[] height_temp = Console.ReadLine().Split(' ');
+        int[] height = Array.ConvertAll(height_temp,Int32.Parse);
+        // your code goes here
+        int result = MagicBevarage(k, height);
+        Console.WriteLine(result);
+        //Console.ReadKey();
     }
+    
+    private static int MagicBevarage(int k, int[] height)
+    {
+        int diff = 0;
 
-    std::cout << (max <= k ? 0 : max - k) << '\n';
-    return 0;
+        for (int i = 0; i < height.Length; i++)
+        {
+            if(k<height[i])
+            {
+                if(diff<height[i]-k)
+                diff = height[i] - k;
+            }
+        }
+
+        return diff;
+    }
 }
