16d0c7f54d2e34953bd25d40fa24e44b76b7d85b
Iti Tyagi <intechworx@gmail.com>
2017-09-22 14:13:35 +0530

    Create UtopianTree.cs

---
+++ UtopianTree.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < t; a0++)
+        {            
+            int n = Convert.ToInt32(Console.ReadLine());
+            int height = 1;
+            for (int i = 1; i <=n; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    height++;
+                }
+                else
+                {
+                    height = 2 * height;
+                }
+            }
+
+            Console.WriteLine(height);
+        }
+        Console.ReadKey();
+    }
+}
