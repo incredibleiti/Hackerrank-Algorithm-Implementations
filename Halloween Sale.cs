be17a6c3014e8b9e5dcd486c47a451427fffc89e
Iti Tyagi <intechworx@gmail.com>
2018-09-25 15:29:43 +0200

    Update Halloween Sale.cs

--- Halloween Sale.cs
+++ Halloween Sale.cs
@@ -1,1 +1,62 @@
 Problem statement: https://www.hackerrank.com/challenges/halloween-sale/problem
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the howManyGames function below.
+    static int howManyGames(int p, int d, int m, int s) {
+        // Return the number of games you can buy
+         int sum = 0;
+            int cnt = 0;
+            while(sum<=s)
+            {
+                cnt += 1;
+                sum += p;
+                if (p-d>=m)
+                {
+                    p = p - d;
+                }
+                else
+                {
+                    p = m;
+                }
+            }
+            return cnt-1;
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] pdms = Console.ReadLine().Split(' ');
+
+        int p = Convert.ToInt32(pdms[0]);
+
+        int d = Convert.ToInt32(pdms[1]);
+
+        int m = Convert.ToInt32(pdms[2]);
+
+        int s = Convert.ToInt32(pdms[3]);
+
+        int answer = howManyGames(p, d, m, s);
+
+        textWriter.WriteLine(answer);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+
